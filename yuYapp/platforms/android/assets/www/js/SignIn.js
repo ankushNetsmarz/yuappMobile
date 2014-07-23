78d655cb45363ca3aa1551322bb9924777773f14
PoojaSharma2142 <pooja.sharma@weexcelindia.in>
2014-07-23 18:22:01 +0530

    logout functionality, local Storages
    implemeted

--- yuYapp/platforms/android/assets/www/js/SignIn.js
+++ yuYapp/platforms/android/assets/www/js/SignIn.js
@@ -1,5 +1,22 @@
-﻿function SignIn(userLoginEmailId, loginPassword) {
-  
+﻿
+$("#loginButton").on("click", function () {
+
+    var userLoginEmailId = $("#userLoginEmailId").val();
+    var loginPassword = $("#userLoginPassword").val();
+
+    if (userLoginEmailId != '' || loginPassword != '')
+    {
+        SignIn(userLoginEmailId, loginPassword);
+    }
+    else {
+        alert("fields cant be empty");
+    }
+});
+
+
+
+function SignIn(userLoginEmailId, loginPassword) {
+
     var userData = {
 
         email: userLoginEmailId,
@@ -7,51 +24,27 @@
     };
     $.ajax({
         type: "GET",
-        //url: "http://localhost:6269/users/login",
-        url: "http://174.141.233.6/YuY/users/login",
+        beforeSend: showLoader(),
+        url: mainUrl + "login",
         data: userData,
         success: function (data) {
-         
+
             console.log(data);
-           
+
             if (data.ResponseData[0].UserID != 0)
             {
+                localStorage.setItem("LoginName", userLoginEmailId);
+                localStorage.setItem("loginPassword", loginPassword);
                 window.location.replace("home.html");
             }
-            else
-            {
+            else {
                 alert("wrong user");
             }
         },
-        error: function (xhr) {        
-            alert(xhr.responseText);
-        }
-    });
-}
-function ValidateEmail() {
-  
-    var postData = {
-        email: $("#SignUpEmail").val()
-    };
-    $.ajax({
-        type: "GET",
-      //  url: "http://localhost:6269/users/checkuser",
-      url: "http://174.141.233.6/YuY/users/checkuser",
-        data: postData,
-        success: function (data) {
-          
-            if (data.ResponseData != 1) {
-
-            }
-            else {
-                alert("already registered");
-            }
-            console.log(data);
-            //alert("success..." + data);
-        },
         error: function (xhr) {
-        
             alert(xhr.responseText);
         }
+    }).done(function () {
+        hideLoader();
     });
 }
